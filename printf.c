7b4b27e2ad8b47c429587547cb759bb2c8b196f5
Adnangad <gardobuyaadnan@gmail.com>
2023-10-16 03:47:49 -0700

    task 0-handles c, s, and %

--- printf.c
+++ printf.c
@@ -9,46 +9,37 @@
 int _printf(const char *format, ...)
 {
         int i;
-	int length = 0;
         char car, *str;
-	char per = '%';
+        int length = 0;
         va_list pr;
 
-	if (format == NULL)
-	{
-		return (1);
-	}
-	va_start(pr, format);
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-			{
-				break;
-			}
-			if (format[i] == 'c')
-			{
-				car = va_arg(pr, int);
-				write(1, &car, 1);
-				length++;
-			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(pr, char *);
-				write(1, str, strlen(str));
-				length += strlen(str);
-			}
-			else if (format[i] == '%')
-			{
-				write(1, &per, 1);
-				length++;
+        va_start(pr, format);
+        for (i = 0; format[i]; i++)
+        {
+                if (format[i] == '%')
+                {
+                        i++;
+                        switch (format[i])
+                        {
+                                case 'c':
+                                        car = va_arg(pr, int);
+                                        putchar(car);
+                                        length++;
+                                        break;
+                                case 's':
+                                        str = va_arg(pr, char *);
+                                        fputs(str, stdout);
+                                        length += strlen(str);
+                                        break;
+                                case '%':
+                                        putchar('%');
+                                        length++;
+                                        break;
 			}
 		}
 		else
 		{
-			write(1, &format[i], 1);
+			putchar(format[i]);
 			length++;
 		}
 	}
