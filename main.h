c79c10a9a6c81ad6860317ca0a2167f92b4f526c
Adnangad <gardobuyaadnan@gmail.com>
2023-10-16 02:36:51 -0700

    task 0-handles s, c and %

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <strlen.h>
 int _printf(const char *format, ...);
-int _putchar(char c);
 #endif
