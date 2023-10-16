48a72cec3edfd3c26776d1b39379e0e8f02e3cc6
Adnangad <gardobuyaadnan@gmail.com>
2023-10-16 02:19:09 -0700

    task 0-handles s, c and %

--- printf.c
+++ printf.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
 
 /**
  * _printf - creates a function like printf
@@ -16,7 +12,6 @@ int _printf(const char *format, ...)
 	int length = 0;
         char car, *str;
 	char per = '%';
-	ssize_t len;
         va_list pr;
 
 	va_start(pr, format);
@@ -28,24 +23,24 @@ int _printf(const char *format, ...)
 			if (format[i] == 'c')
 			{
 				car = va_arg(pr, int);
-				write(STDOUT_FILENO, &car, 1);
+				write(1, &car, 1);
 				length++;
 			}
 			else if (format[i] == 's')
 			{
 				str = va_arg(pr, char *);
-				len = write(STDOUT_FILENO, str, strlen(str));
-				length += len;
+				write(1, str, strlen(str));
+				length += strlen(str);
 			}
 			else if (format[i] == '%')
 			{
-				write(STDOUT_FILENO, &per, 1);
+				write(1, &per, 1);
 				length++;
 			}
 		}
 		else
 		{
-			write(STDOUT_FILENO, &format[i], 1);
+			write(1, &format[i], 1);
 			length++;
 		}
 	}
