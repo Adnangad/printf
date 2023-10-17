3f65e52cb87c998a713198b7d7646ef1123efa0b
Adnangad <gardobuyaadnan@gmail.com>
2023-10-16 23:36:08 -0700

    task-0

--- 0-printf.c
+++ 0-printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	}
         va_start(pr, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
         {
 		if (format[i] != '%')
 		{
